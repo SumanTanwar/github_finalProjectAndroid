bca4d692a139304ff2b19e6cfd63fca026b8ea0d
Suman Tanwar <stanwar2030@gmail.com>
2024-06-23 01:55:44 -0400

    This is my Second commit

--- app/src/main/java/com/example/finalprojectandroid/RunningActivity.java
+++ app/src/main/java/com/example/finalprojectandroid/RunningActivity.java
@@ -1,5 +1,7 @@
 package com.example.finalprojectandroid;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class RunningActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/finalprojectandroid/Skipping.java
+++ app/src/main/java/com/example/finalprojectandroid/Skipping.java
@@ -4,19 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Skipping extends AppCompatActivity {
 
 
     Button btnMainSkip;
+
+    private TextView tvTimer;
+    private Button btnStart, btnPause, btnReset;
+
+    private Handler handler = new Handler();
+    private long startTime = 0L;
+    private long timeInMillis = 0L;
+    private long timeSwapBuff = 0L;
+    private long updateTime = 0L;
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMillis = System.currentTimeMillis() - startTime;
+            updateTime = timeSwapBuff + timeInMillis;
+            int secs = (int) (updateTime / 1000);
+            int mins = secs / 60;
+            int hrs = mins / 60;
+            secs = secs % 60;
+            tvTimer.setText(String.format("%02d:%02d:%02d", hrs, mins, secs));
+            handler.postDelayed(this, 0);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skipping);
 
         btnMainSkip =findViewById(R.id.btnMainSkip);
+
+        tvTimer = findViewById(R.id.timerSkip);
+        btnStart = findViewById(R.id.btnStartSkip);
+        btnPause = findViewById(R.id.btnPauseSkip);
+        btnReset = findViewById(R.id.btnResetSkip);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = System.currentTimeMillis();
+                handler.postDelayed(updateTimerThread, 0);
+                btnStart.setEnabled(false);
+                btnPause.setEnabled(true);
+                btnReset.setEnabled(true);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff += timeInMillis;
+                handler.removeCallbacks(updateTimerThread);
+                btnStart.setEnabled(true);
+                btnPause.setEnabled(false);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = 0L;
+                timeInMillis = 0L;
+                timeSwapBuff = 0L;
+                updateTime = 0L;
+                tvTimer.setText("00:00:00");
+                handler.removeCallbacks(updateTimerThread);
+                btnStart.setEnabled(true);
+                btnPause.setEnabled(false);
+                btnReset.setEnabled(false);
+            }
+        });
 
 
         btnMainSkip.setOnClickListener(new View.OnClickListener() {
